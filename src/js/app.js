522f37073a0ad69651d1d4f34e11f47b369dcea5
Yana <yana.samusenko@internet.ru>
2024-08-27 16:52:13 +0300

    func js

--- src/js/app.js
+++ src/js/app.js
@@ -1,49 +1,57 @@
 import * as bootstrap from "bootstrap";
 
-const root = document.querySelector("#root");
+const modalBasket = document.querySelector("#exampleModal");
+const modalCard = document.querySelector("#cardsModal");
 
-const carousel = new bootstrap.Carousel("#carouselExample");
+// Хранение данных
+let cards = [];
 
-const cardProduct = document.querySelector(".card-product");
+// Загрузка карточек
+fetch("https://fakestoreapi.com/products")
+  .then((response) => response.json())
+  .then((json) => (cards = json))
+  .then(() => renderCards(cards))
+  .then(() => openModalCard());
 
-async function fetchAndDisplayProducts() {
-  const apiUrl = "https://fakestoreapi.com/products";
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  return data;
+// Функция создания карточек
+function createCard(card) {
+  return `<div class="cards__card" id="${card.id}" style="width: 18rem;">
+      <img src="${card.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${card.title}</h5>
+        <p class="card-text">${card.price}</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">${card.description}</li>
+        <li class="list-group-item">${card.category}</li>
+      </ul>
+      <div class="card-body">
+        <button type="button" id="add-to-cart" class="btn btn-primary">Добавить в корзину</button>
+      </div>
+    </div>`;
 }
 
-function createCardProduct(product) {
-  const { title, price, description, image, category } = product;
-  return `<div class="card" style="width: 18rem;">
-  <img src=${image} class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${title}</h5>
-    <p class="card-text">${price}</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">${description}</li>
-    <li class="list-group-item">${category}</li>
-    </ul>
-  <div class="card-body">
-    <button type="button" id="add-to-cart" class="btn btn-primary">Добавить в корзину</button>
-  </div>
-</div>`;
+// Функция отображения карточек
+function renderCards(cards) {
+  const cardsContainer = document.querySelector(".cards__container");
+  cardsContainer.innerHTML = "";
+
+  cards.forEach((card) => {
+    cardsContainer.innerHTML += createCard(card);
+  });
 }
 
-const renderCard = (card) => (cardProduct.innerHTML += createCardProduct(card));
-const renderCards = (products) =>
-  products.forEach((product) => renderCard(product));
+// Функция открытия модального окна для карточек
 
-fetchAndDisplayProducts().then((products) => renderCards(products));
-
-const addButtons = document.querySelectorAll(".card #add-to-cart");
-
-addButtons.forEach((button) =>
-  button.addEventListener("click", (e) => {
-    e.preventDefault();
-    myModalCard.show();
-  })
-);
-// const myModalBasket = new bootstrap.Modal("#modal__basket", options);
-// const myModalCard = bootstrap.Modal("#exampleModal", options);
+function openModalCard() {
+  const myModal = new bootstrap.Modal(modalCard);
+  const addButtons = document.querySelectorAll(".cards__card");
+  addButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      myModal.show();
+      modalCard.querySelector(".modal-body").innerHTML = createCard(
+        cards[button.id - 1]
+      );
+    });
+  });
+}
